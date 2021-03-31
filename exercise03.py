fb22bd811bdfa5b50676762d691154023d373639
Egoh390 <80371372+Egoh390@users.noreply.github.com>
2021-03-31 15:08:19 +1300

    Update exercise03.py
    Ex03 completed

--- exercise03.py
+++ exercise03.py
@@ -1,4 +1,6 @@
 # Exercise Three
 # Write a simple program that finds the number of digits of a given integer value
 
-print("The number of digits for a value of 100 is 3")
+number = 100
+stringNumber = str(abs(number))
+print ('The length of ' + str(number) + ' is ' + str(len(stringNumber)))
