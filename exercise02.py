8cacf00528fb78db7d313f66d963741431911439
pbol970 <79953088+pbol970@users.noreply.github.com>
2021-03-31 14:49:35 +1300

    Update exercise02.py
    Done Ex2

--- exercise02.py
+++ exercise02.py
@@ -2,3 +2,13 @@
 # Write a simple program that finds the value at the nth position in the Fibonacci sequence
 
 print("The value at the 6th postion in the Fibonacci sequence is 8")
+
+def fibonacci(n): 
+    if n == 0: 
+        return 0
+    if n == 1: 
+        return 1
+    else: 
+        return fibonacci(n-1)+fibonacci(n-2)
+
+print(fibonacci(6))
